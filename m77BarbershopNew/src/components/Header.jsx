5b7711a356867bddedb504d43e4cc7d7051f094a
RaduStefanLungu <radustefanlungu@hotmail.com>
2024-12-01 21:57:55 +0100

    > created burger menu for phone > added real logo to header

--- m77BarbershopNew/src/components/Header.jsx
+++ m77BarbershopNew/src/components/Header.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { TbMenu2,TbMenuDeep  } from "react-icons/tb";
+
+import LOGO from '../assets/logo/logo.jpg'
+
+
 export default function Header() {
+
+  const [burgerClicked,setBurgerClicked] = useState(false);
+
+  function handleClickedBurger(e){
+    e.preventDefault();
+
+    setBurgerClicked(!burgerClicked);
+
+  }
+
   return (
-    <header className='fixed z-[100] w-screen flex justify-between bg-[var(--brand-white)] py-3 px-5 xl:px-10'>
+    <header className='fixed z-[100] w-screen flex flex-col md:flex-row justify-between bg-[var(--brand-black)] py-3 md:px-5 xl:px-10'>
 
 
-      <div className=''>
-        <div id='img' className='w-[125px] h-[75px] bg-slate-600' />
+      <div className='grid justify-center items-center pb-2 md:pb-0'>
+        <img id='img' src={LOGO} className='w-[120px] h-[90px] bg-slate-600' />
       </div>
 
 
-      <div className='flex items-center gap-2 '>
+      <div className='relative flex flex-col md:flex-row'>
 
-        <HeaderButton Text={"Acceuil"} Path={"/"} />
-        <HeaderButton Text={"A propos"} Path={"/a-propos"} />
-        <HeaderButton Text={"Gallerie"} Path={"/gallerie"} />
-        <HeaderButton Text={"Rendez-vous"} Path={"/rendez-vous"} />
+        <button onClick={handleClickedBurger} className='md:hidden grid justify-center items-center'>
+          {
+            burgerClicked? 
+            <TbMenuDeep className='text-4xl text-[var(--brand-white)]'/> :
+              <TbMenu2 className='text-4xl text-[var(--brand-white)]'/>
+          }
+        </button>
+
+        <div className={`absolute w-screen ${burgerClicked? " top-10 translate-y-0 duration-700 transition-transform" : "-top-32 -translate-y-full transition-transform duration-500"} md:top-0 md:translate-y-0 md:duration-0 md:relative md:w-auto grid text-center md:flex items-center gap-2 bg-[var(--brand-black)]`}>
+          <HeaderButton Text={"Acceuil"} Path={"/"} />
+          <HeaderButton Text={"A propos"} Path={"/a-propos"} />
+          <HeaderButton Text={"Gallerie"} Path={"/gallerie"} />
+          <HeaderButton Text={"Rendez-vous"} Path={"/rendez-vous"} />
+        </div>
 
         {/* TODO : create burger */}
 
